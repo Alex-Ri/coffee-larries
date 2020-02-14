f85ac4e7eba77f99eab37d7edeec0b01ed6576a8
knitzie <anja.dehn@projekteins.biz>
2020-02-14 19:11:15 +0100

    add coffee form added

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -10,6 +10,7 @@ import Admin from '../views/Admin.vue'
 import UserList from '../views/UserList.vue'
 import Confirmation from '../views/Confirmation.vue'
 import AddMe from '../views/AddMe.vue'
+import AddCoffee from '../views/AddCoffee.vue'
 
 Vue.use(VueRouter)
 
@@ -72,6 +73,11 @@ const routes = [
 	// 	// this generates a separate chunk (about.[hash].js) for this route
 	// 	// which is lazy-loaded when the route is visited.
 	// 	component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+	},
+	{
+		path: '/admin/addcoffee',
+		name: 'AddCoffee',
+		component: AddCoffee
 	}
 ]
 
